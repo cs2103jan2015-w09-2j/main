35b52aa02bce7a57a60db3ed7bd4e3d6ec6d65fc
Zhi Hua <ngzhihua_@hotmail.com>
2015-03-03 01:49:10 +0800

    Task Serializer class

---
+++ src/TaskSerializer.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+
+
+public class TaskSerializer implements JsonSerializer<Task> {
+    public JsonElement serialize(final Task task, final Type type, final JsonSerializationContext context) {
+        JsonObject result = new JsonObject();
+        result.add("description", new JsonPrimitive(task.getDescription()));
+        result.add("start", new JsonPrimitive(task.getStart().toString()));
+        result.add("end" , new JsonPrimitive(task.getEnd().toString()));
+        return result;
+    }
+}
