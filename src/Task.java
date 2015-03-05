08a64ebc82a262b6f6d07d947dcfc82313b79744
Zhi Hua <ngzhihua_@hotmail.com>
2015-03-05 18:01:11 +0800

    Stash Task

--- src/Task.java
+++ src/Task.java
@@ -9,8 +9,8 @@ public class Task{
 	
 	public Task(int startYear, int startMonth, int startDay, int startHour, int startMin, int endYear, int endMonth, int endDate, int endHour, int endMin, String userDescription){
 		description = userDescription;
-		start = new GregorianCalendar();
-		end = new GregorianCalendar();
+		start = Calendar.getInstance();
+		end = Calendar.getInstance();
 		start.set(Calendar.YEAR, startYear);
 		start.set(Calendar.MONTH, startMonth);
 		start.set(Calendar.DAY_OF_MONTH, startDay);
