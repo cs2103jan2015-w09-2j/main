5351c30c1a671fc8efaa7fd2c56460f249a75a13
Zhi Hua <ngzhihua_@hotmail.com>
2015-03-11 00:32:57 +0800

    Changed the way null values are handled in both serializer and deserializer.

--- src/TaskDeserializer.java
+++ src/TaskDeserializer.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -8,7 +9,9 @@ import com.google.gson.JsonObject;
 
 public class TaskDeserializer implements JsonDeserializer<Task> {
 
-  @Override
+  private static final String EMPTY_STRING = "";
+
+@Override
   public Task deserialize(final JsonElement json, final Type typeOfT, final JsonDeserializationContext context){
 	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 	if (json == null){
@@ -22,9 +25,12 @@ public class TaskDeserializer implements JsonDeserializer<Task> {
     
     final Task task = new Task();
     task.setDescription(description);
-    task.setStart(LocalDateTime.parse(start, formatter));
+    if (!start.equals(EMPTY_STRING)){
+    	task.setStart(LocalDateTime.parse(start, formatter));
+    }
+    if (!end.equals(EMPTY_STRING)){
     task.setEnd(LocalDateTime.parse(end, formatter));
-    
+    }
     return task;
   }
 }
--- src/TaskSerializer.java
+++ src/TaskSerializer.java
@@ -12,18 +12,29 @@ import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
 public class TaskSerializer implements JsonSerializer<Task> {
+	private static final String EMPTY_STRING = "";
+
 	public JsonElement serialize(final Task task, final Type type,
 			final JsonSerializationContext context) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 		
 		JsonObject result = new JsonObject();
 		result.add("Description", new JsonPrimitive(task.getDescription()));
-		if (task.getStart() != null){
+		
+		if (task.getStart() == null){
+			result.add("start", new JsonPrimitive(EMPTY_STRING));
+		}
+		else{
 		result.add("start", new JsonPrimitive(task.getStart().format(formatter).toString()));
 		}
-		if (task.getEnd() != null){
+		
+		if (task.getEnd() == null){
+			result.add("end", new JsonPrimitive(EMPTY_STRING));
+		}
+		else{
 		result.add("end", new JsonPrimitive(task.getEnd().format(formatter).toString()));
 		}
+		
 		return result;
 	}
 }
