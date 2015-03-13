9404f45d3433564b471b0afa86a4a3237a3c0dbd
SSrivatsan <SSrivatsan@users.noreply.github.com>
2015-03-13 14:34:18 +0800

    Update Parser.java

--- src/Parser.java
+++ src/Parser.java
@@ -28,20 +28,20 @@ public class Parser {
 		    int[] startTime = new int[5];
 		    int[] endTime = new int[5];
 		    
-		    if(input1.charAt(2) == '/')
+		    if(userInput.charAt(2) == '/')
 		    {
 		    	startTime = convertToInt(stringStartTime);
-		    	return Task(startTime[0], startTime[1],startTime[2], startTime[3], startTime[4], -1,-1,-1,-1,null);
+		    	return new Task(startTime[0], startTime[1],startTime[2], startTime[3], startTime[4], -1,-1,-1,-1,null);
 		    }
 		   // String test = "pick up boiboi from 23/01/2015 23:15 to 24/01/2015 13:14" ;
-		    String[] content = test.split("from");
+		    String[] content = userInput.split("from");
 		    description = content[0].trim();
 		    content = content[1].split("to");
 		    stringStartTime = content[0].trim();
 		    stringEndTime = content[1].trim();
 		    startTime = convertToInt(stringStartTime);
 		    endTime = convertToInt(stringEndTime);
-		    return Task(startTime[0], startTime[1],startTime[2], startTime[3], startTime[4], endTime[0], endTime[1],endTime[2], endTime[3], endTime[4], description);
+		    return new Task(startTime[0], startTime[1],startTime[2], startTime[3], startTime[4], endTime[0], endTime[1],endTime[2], endTime[3], endTime[4], description);
 		  }
 	private static int[] convertToInt(String stringTime){
 		    int[] intTime = new int[5];
