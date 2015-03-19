1e67baebdfd613abc9026b8d0b307b5518b08d26
Gangadevi <gangadevi.balakrishnan@gmail.com>
2015-03-19 22:49:56 +0800

    changed from JTextField to JTextPane to allow colouring of command as user enters

--- src/HomeView.java
+++ src/HomeView.java
@@ -1,83 +1,58 @@
 //@author A0112715R
 
 import java.awt.Color;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JTextPane;
 
 import java.awt.Font;
 
 import javax.swing.border.EtchedBorder;
+import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 import javax.swing.text.StyledDocument;
 import javax.swing.JPanel;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class HomeView {
 
 	private JFrame frame;
-	private JTextField commandFromUser;
+	private JTextPane commandFromUser;
 	private JTextPane showToUser;
 	private Display display = Display.getInstanceOfDisplay();
+	private Controller control;
+
 	/**
 	 * Launch the application here.
+	 * 
+	 * @throws BadLocationException
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					HomeView window = new HomeView();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	public static void main(String[] args) throws BadLocationException {
+		HomeView window = new HomeView();
+		window.frame.setVisible(true);
+
 	}
 
-	public HomeView() throws BadLocationException {
+	private HomeView() throws BadLocationException {
 		initialize();
 	}
 
 	private void initialize() throws BadLocationException {
 		designOfInterface();
-		Controller control = new Controller();
+		control = new Controller();
 		control.executeCommand("display 1");
 		displayHomeView();
-
-		commandFromUser.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-
-				String command = commandFromUser.getText();
-					control.executeCommand(command);
-				
-
-				try {
-					displayHomeView();
-				} catch (BadLocationException badlocation) {
-					JOptionPane.showMessageDialog(null, "Error Message!");
-				}
-
-				commandFromUser.setText("");
-
-			}
-
-		});
 	}
 
-
-	public void displayHomeView() throws BadLocationException {
+	private void displayHomeView() throws BadLocationException {
 		// Colouring and styling of text
-	
 		showToUser.setText("");
 		StyledDocument doc = showToUser.getStyledDocument();
 		Style style = showToUser.addStyle("Style", null);
@@ -96,24 +71,38 @@ public class HomeView {
 		doc.insertString(doc.getLength(), "\nSomeday: \n", style);
 		StyleConstants.setForeground(style, Color.BLACK);
 		doc.insertString(doc.getLength(), display.getSomeday(), style);
-		
-		
+
 	}
 
-	public void designOfInterface() {
+	private void designOfInterface() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setBounds(100, 100, 628, 464);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		commandFromUser = new JTextField();
+		commandFromUser = new JTextPane();
+		commandFromUser.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(java.awt.event.KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					String command = commandFromUser.getText();
+					control.executeCommand(command);
+
+					try {
+						displayHomeView();
+					} catch (BadLocationException badlocation) {
+						JOptionPane.showMessageDialog(null, "Error Message!");
+					}
+					commandFromUser.setText("");
+				}
+			}
+		});
 		commandFromUser.setFont(new Font("Calibri", Font.PLAIN, 20));
 		commandFromUser.setBackground(new Color(255, 255, 255));
 
 		commandFromUser.setBounds(20, 371, 573, 43);
 		frame.getContentPane().add(commandFromUser);
-		commandFromUser.setColumns(10);
 		showToUser = new JTextPane();
 		showToUser.setFont(new Font("Calibri", Font.PLAIN, 20));
 		showToUser.setBackground(new Color(255, 255, 255));
@@ -133,4 +122,6 @@ public class HomeView {
 				144, 255), new Color(0, 0, 0)));
 		frame.getContentPane().add(panel);
 	}
+
+	
 }
