544a8bb3cea65dc8d70f7077fa46b50457c04452
SSrivatsan <SSrivatsan@users.noreply.github.com>
2015-03-23 21:54:00 +0800

    Update HelpCmd.java

--- src/HelpCmd.java
+++ src/HelpCmd.java
@@ -1,10 +1,15 @@
 
 public class HelpCmd extends Cmd {
-
+	
+	public HelpCmd(){
+		System.out.println("In HelpCmd");
+	}
 	@Override
-	public Display execute() {
+	public Display execute(Data myList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 }
+
+
