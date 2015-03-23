133901ba73377e9eb6388d619cdb1f728829af9e
SSrivatsan <SSrivatsan@users.noreply.github.com>
2015-03-23 21:36:33 +0800

    Update ViewCmd.java
    Added a comment for print

--- src/ViewCmd.java
+++ src/ViewCmd.java
@@ -4,13 +4,12 @@ public class ViewCmd extends Cmd{
 	String view;
 	
 	public ViewCmd(String view) {
+	//	System.out.println("View is in ViewCmd");
 		this.view = view;
 		// TODO Auto-generated constructor stub
 	}
 
-	public Display execute(){
-		Data myList = Data.getInstance();
-		
-	     return new Display(View.getInstance(), "");
+	public Display execute(Data myList){
+	     return new Display(myList.toDateView(), "");
 	}
 }
