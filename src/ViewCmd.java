7ec9b5aec93f67d478688ff74ff11a91637c8dba
tanch88 <chunhow88@hotmail.com>
2015-03-23 23:28:59 +0800

    Revert "Update ViewCmd.java"
    This reverts commit 133901ba73377e9eb6388d619cdb1f728829af9e.

--- src/ViewCmd.java
+++ src/ViewCmd.java
@@ -4,12 +4,13 @@ public class ViewCmd extends Cmd{
 	String view;
 	
 	public ViewCmd(String view) {
-	//	System.out.println("View is in ViewCmd");
 		this.view = view;
 		// TODO Auto-generated constructor stub
 	}
 
-	public Display execute(Data myList){
-	     return new Display(myList.toDateView(), "");
+	public Display execute(){
+		Data myList = Data.getInstance();
+		
+	     return new Display(View.getInstance(), "");
 	}
 }
