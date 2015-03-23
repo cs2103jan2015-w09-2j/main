e166934b2671a6b4701c53185810233518a601cc
tanch88 <chunhow88@hotmail.com>
2015-03-23 12:55:21 +0800

    DateView: Singleton

--- src/DateView.java
+++ src/DateView.java
@@ -2,21 +2,33 @@ import java.util.ArrayList;
 
 
 public class DateView extends View{
-
-	ArrayList<Task> today;
-	ArrayList<Task> upcoming;
-	ArrayList<Task> someday;
 	
-	public DateView(){
-		this.today = null;
-		this.upcoming = null;
-		this.someday = null;
+	private static DateView dateView = null;
+	private ArrayList<Task> today;
+	private ArrayList<Task> upcoming;
+	private ArrayList<Task> someday;
+	
+	private DateView(){
+		this.today = new ArrayList<Task>();
+		this.upcoming = new ArrayList<Task>();
+		this.someday = new ArrayList<Task>();
 	}
 	
-	public DateView(ArrayList<Task> today, ArrayList<Task> upcoming, ArrayList<Task> someday) {
+	private DateView(ArrayList<Task> today, ArrayList<Task> upcoming, ArrayList<Task> someday) {
 		this.today = today;
 		this.upcoming = upcoming;
 		this.someday = someday;
+	}
+	
+	public static DateView getInstance(){
+		if(dateView == null){
+			dateView = new DateView();
+		}
+		return dateView;
+	}
+	
+	public static void setInstance(ArrayList<Task> today, ArrayList<Task> upcoming, ArrayList<Task> someday){
+		dateView = new DateView(today, upcoming, someday);
 	}
 
 	public ArrayList<Task> getToday() {
