9decce5db63dc33e5ea23c2c148847cad5d28b17
Zhi Hua <ngzhihua_@hotmail.com>
2015-03-24 21:01:39 +0800

    Reupload SomedayView after merge conflict disaster

---
+++ src/SomedayView.java
@@ -0,0 +1,27 @@
+import javax.swing.text.BadLocationException;
+
+
+public class SomedayView implements View{
+
+	public SomedayView(){
+	}
+	
+	@Override
+	public Task getTask(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() throws BadLocationException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
