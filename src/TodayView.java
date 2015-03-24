e920b16e85ed78de8d35cae95d05c8e2720583d3
Gangadevi <gangadevi.balakrishnan@gmail.com>
2015-03-24 19:54:35 +0800

    view for today

--- src/TodayView.java
+++ src/TodayView.java
@@ -1,8 +1,15 @@
+import java.awt.Color;
+
+import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 
 public class TodayView implements View{
 
+	DateView today = new DateView();
 	public TodayView(){
 	}
 
@@ -21,7 +28,17 @@ public class TodayView implements View{
 	@Override
 	public void show() throws BadLocationException {
 		// TODO Auto-generated method stub
+		DateView today = new DateView();
+		UserInterface UI = UserInterface.getInstance();
+		JTextPane showToUser = UI.getShowToUser();
 		
+		StyledDocument doc = showToUser.getStyledDocument();
+		Style style = showToUser.addStyle("Style", null);
+		StyleConstants.setForeground(style, Color.BLUE.brighter());
+		doc.insertString(doc.getLength(), " Today: \n", style);
+		
+		StyleConstants.setForeground(style, Color.BLACK);
+		doc.insertString(doc.getLength(), today.getToday(), style);
 	}
 
 }
