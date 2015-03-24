514774c087f85fe84b3bd511011ef0e7ad913ffe
gangadevib <gangadevi.balakrishnan@gmail.com>
2015-03-25 03:05:16 +0800

    Someday view updated to support edit and delete

--- src/SomedayView.java
+++ src/SomedayView.java
@@ -1,6 +1,11 @@
+import java.awt.Color;
 import java.util.ArrayList;
 
+import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 
 public class SomedayView implements View{
@@ -10,9 +15,10 @@ public class SomedayView implements View{
 	public SomedayView(){
 		update();
 	}
-	
+
 	@Override
 	public Task getTask(int index) {
+		index--;
 		return someday.get(index);
 	}
 
@@ -20,13 +26,33 @@ public class SomedayView implements View{
 	public void update() {
 		Data data = Data.getInstance();
 		
-		someday = data.getSomeday();
+		this.someday = data.getSomeday();
+		
+	}
+	
+	protected String getSomedayTask() {
+		String tasks = "";
+		int i =0;
+		for (Task task : someday) {
+			i++;
+			tasks += "  "+i + ". " + task.toString() + "\n";
+		}
+		return tasks;
 	}
 
 	@Override
 	public void show() throws BadLocationException {
 		// TODO Auto-generated method stub
+		UserInterface UI = UserInterface.getInstance();
+		JTextPane showToUser = UI.getShowToUser();
 		
+		StyledDocument doc = showToUser.getStyledDocument();
+		Style style = showToUser.addStyle("Style", null);
+		StyleConstants.setForeground(style, Color.BLUE.brighter());
+		doc.insertString(doc.getLength(), " Someday: \n", style);
+		
+		StyleConstants.setForeground(style, Color.BLACK);
+		doc.insertString(doc.getLength(), getSomedayTask(), style);
 	}
 
 }
