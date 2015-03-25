6eacedc11fc0f5be0f1f64c969d501d371bf46dc
Gangadevi <gangadevi.balakrishnan@gmail.com>
2015-03-25 15:40:51 +0800

    Implemented Search view

--- src/SearchView.java
+++ src/SearchView.java
@@ -1,6 +1,11 @@
+import java.awt.Color;
 import java.util.ArrayList;
 
+import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 
 public class SearchView implements View{
@@ -12,15 +17,35 @@ public class SearchView implements View{
 		
 		searchedList = data.getSearched(searchedText);
 	}
+	
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	protected String getTask() {
+		String tasks = "";
+		int i =0;
+		for (Task task : searchedList) {
+			i++;
+			tasks += "  "+i + ". " + task.toString() + "\n";
+		}
+		return tasks;
+	}
+
 	@Override
 	public void show() throws BadLocationException {
-		// TODO Auto-generated method stub
+		UserInterface UI = UserInterface.getInstance();
+		JTextPane showToUser = UI.getShowToUser();
+		
+		StyledDocument doc = showToUser.getStyledDocument();
+		Style style = showToUser.addStyle("Style", null);
+		StyleConstants.setForeground(style, Color.BLUE.brighter());
+		doc.insertString(doc.getLength(), " Search Results: \n", style);
+		
+		StyleConstants.setForeground(style, Color.BLACK);
+		doc.insertString(doc.getLength(), getTask(), style);
 		
 	}
 
