3e3520f0a52ac0afd260c859c71b0f49db5f471b
Gangadevi <gangadevi.balakrishnan@gmail.com>
2015-03-29 16:38:33 +0800

    Refactored code

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,42 +1,23 @@
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GraphicsEnvironment;
-import java.awt.Image;
-import java.awt.SystemTray;
-import java.awt.Toolkit;
-import java.awt.TrayIcon;
-
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
-
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 import javax.swing.text.StyledDocument;
-
-import java.awt.Button;
-
 import javax.swing.JButton;
-
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
 import javax.swing.UIManager;
 import javax.swing.JLabel;
 
@@ -48,10 +29,6 @@ public class UserInterface {
 	private static Controller control;
 	private static JPanel outerPanel;
 	private DefaultStyledDocument doc = new DefaultStyledDocument();
-
-	/**
-	 * Launch the application.
-	 */
 	private static UserInterface UI = null;
 	private JPanel feedbackPanel;
 	private JPanel topPanel;
@@ -97,6 +74,24 @@ public class UserInterface {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(550, 540);
+
+		initializeCmdFromUser();
+		initializeShowToUser();
+		initializeFeedback();
+		initializeCloseButton();
+		initializeMinimizeButton();
+		initializeWelcomeLabel();
+		getCommand();
+	}
+
+	private void initializeWelcomeLabel() {
+		welcomeLabel = new JLabel(" OneTag", SwingConstants.LEFT);
+		welcomeLabel.setFont(new Font("Elephant", Font.PLAIN, 16));
+		welcomeLabel.setForeground(new Color(255, 255, 255));
+		topPanel.add(welcomeLabel, BorderLayout.CENTER);
+	}
+
+	private void initializeCmdFromUser() {
 		outerPanel = new JPanel();
 		frame.getContentPane().add(outerPanel, BorderLayout.CENTER);
 		outerPanel.setLayout(new BorderLayout(0, 0));
@@ -106,8 +101,7 @@ public class UserInterface {
 				30, 144, 255), new Color(0, 0, 0)));
 		colourRestrictedWords();
 		commandFromUser = new JTextPane(doc);
-
-		commandFromUser.setToolTipText("Enter command here");;
+		commandFromUser.setToolTipText("Enter command here");
 		outerPanel.add(commandFromUser, BorderLayout.SOUTH);
 		commandFromUser.setFont(new Font("Calibri", Font.PLAIN, 20));
 		commandFromUser.setBackground(new Color(255, 255, 255));
@@ -115,14 +109,10 @@ public class UserInterface {
 		commandFromUser.setForeground(new Color(0, 0, 0));
 		commandFromUser.setBorder(new EtchedBorder(EtchedBorder.LOWERED,
 				new Color(30, 144, 255), new Color(0, 0, 0)));
-		topPanel = new JPanel();
-		topPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED,
-				new Color(30, 144, 255), new Color(0, 0, 0)));
-		topPanel.setBackground(new Color(34, 77, 124));
 
-		outerPanel.add(topPanel, BorderLayout.NORTH);
-		topPanel.setLayout(new BorderLayout(0, 0));
+	}
 
+	private void initializeShowToUser() {
 		feedbackPanel = new JPanel();
 		outerPanel.add(feedbackPanel, BorderLayout.CENTER);
 		feedbackPanel.setLayout(new BorderLayout(0, 0));
@@ -134,69 +124,76 @@ public class UserInterface {
 		showToUser.setForeground(new Color(0, 0, 128));
 		showToUser.setBorder(null);
 		showToUser.setBounds(20, 10, 573, 350);
+	}
 
+	private void initializeFeedback() {
 		feedback = new JTextPane();
 		feedback.setForeground(new Color(153, 0, 153));
 		feedback.setFont(new Font("Lucida Fax", Font.BOLD | Font.ITALIC, 15));
 		feedback.setEditable(false);
 		feedbackPanel.add(feedback, BorderLayout.SOUTH);
-		
-				
-				buttonPanel = new JPanel();
-				topPanel.add(buttonPanel, BorderLayout.EAST);
-				buttonPanel.setLayout(new BorderLayout(0, 0));
-				
-				closeButton = new JButton("X");
-				buttonPanel.add(closeButton, BorderLayout.EAST);
-				closeButton.setFont(new Font("Calibri", Font.BOLD, 15));
-				closeButton.setBorder(UIManager.getBorder("Button.border"));
-				closeButton.setBackground(new Color(255, 255, 255));
-				closeButton.setForeground(new Color(0, 0, 128));
-				
-				minimiseButton = new JButton("-");
-				minimiseButton.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseClicked(MouseEvent arg0) {
-					//	minimizeApplication();
-					}
-				});
-				buttonPanel.add(minimiseButton, BorderLayout.WEST);
-				buttonPanel.setBorder(UIManager.getBorder("TextArea.border"));
-				minimiseButton.setFont(new Font("Calibri", Font.BOLD, 17));
-				minimiseButton.setBackground(new Color(255, 255, 255));
-				minimiseButton.setForeground(new Color(0, 0, 128));
-				
-				welcomeLabel = new JLabel(" OneTag",SwingConstants.LEFT);
-				welcomeLabel.setFont(new Font("Elephant", Font.PLAIN , 16));
-				welcomeLabel.setForeground(new Color(255, 255, 255));
-				topPanel.add(welcomeLabel, BorderLayout.CENTER);
-				closeButton.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseClicked(MouseEvent arg0) {
-						System.exit(0);
-					}
-				});
-		getCommand();
 	}
 
-	//minimize app to SystemTray
-//	protected void minimizeApplication() {
-//		// TODO Auto-generated method stub
-//		 TrayIcon trayIcon = null;
-//	     if (SystemTray.isSupported()) {
-//	         // get the SystemTray instance
-//	         SystemTray tray = SystemTray.getSystemTray();
-//	         // load an image
-//	         Image image = Toolkit.getDefaultToolkit().getImage("");
-//	         // create a action listener to listen for default action executed on the tray icon
-//	         ActionListener listener = new ActionListener() {
-//	             public void actionPerformed(ActionEvent e) {
-//	                 // execute default action of the application
-//	                 // ...
-//	             }
-//	         };
-//		
-//	}
+	private void initializeCloseButton() {
+		topPanel = new JPanel();
+		topPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(30,
+				144, 255), new Color(0, 0, 0)));
+		topPanel.setBackground(new Color(34, 77, 124));
+		outerPanel.add(topPanel, BorderLayout.NORTH);
+		topPanel.setLayout(new BorderLayout(0, 0));
+
+		buttonPanel = new JPanel();
+		topPanel.add(buttonPanel, BorderLayout.EAST);
+		buttonPanel.setLayout(new BorderLayout(0, 0));
+
+		closeButton = new JButton("X");
+		buttonPanel.add(closeButton, BorderLayout.EAST);
+		closeButton.setFont(new Font("Calibri", Font.BOLD, 15));
+		closeButton.setBorder(UIManager.getBorder("Button.border"));
+		closeButton.setBackground(new Color(255, 255, 255));
+		closeButton.setForeground(new Color(0, 0, 128));
+		closeButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				System.exit(0);
+			}
+		});
+	}
+
+	private void initializeMinimizeButton() {
+		minimiseButton = new JButton("-");
+		buttonPanel.add(minimiseButton, BorderLayout.WEST);
+		buttonPanel.setBorder(UIManager.getBorder("TextArea.border"));
+		minimiseButton.setFont(new Font("Calibri", Font.BOLD, 17));
+		minimiseButton.setBackground(new Color(255, 255, 255));
+		minimiseButton.setForeground(new Color(0, 0, 128));
+		minimiseButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// minimizeApplication();
+			}
+		});
+	}
+
+	// minimize app to SystemTray
+	// protected void minimizeApplication() {
+	// // TODO Auto-generated method stub
+	// TrayIcon trayIcon = null;
+	// if (SystemTray.isSupported()) {
+	// // get the SystemTray instance
+	// SystemTray tray = SystemTray.getSystemTray();
+	// // load an image
+	// Image image = Toolkit.getDefaultToolkit().getImage("");
+	// // create a action listener to listen for default action executed on the
+	// tray icon
+	// ActionListener listener = new ActionListener() {
+	// public void actionPerformed(ActionEvent e) {
+	// // execute default action of the application
+	// // ...
+	// }
+	// };
+	//
+	// }
 
 	private void getCommand() {
 		commandFromUser.addKeyListener(new KeyAdapter() {
@@ -204,21 +201,22 @@ public class UserInterface {
 			public void keyPressed(java.awt.event.KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 					String command = commandFromUser.getText();
-					String message ="";
+					String message = "";
 					control.executeCommand(command);
 					showToUser.setText("");
 					StyledDocument doc = feedback.getStyledDocument();
 					SimpleAttributeSet center = new SimpleAttributeSet();
-					StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
-					doc.setParagraphAttributes(0, doc.getLength(), center, false);
+					StyleConstants.setAlignment(center,
+							StyleConstants.ALIGN_CENTER);
+					doc.setParagraphAttributes(0, doc.getLength(), center,
+							false);
 					message = Display.getInstance().getMessage();
-					try{
-					if(!message.isEmpty()){
-					feedback.setText((message) + "\n");
-					}
-					}
-					catch(NullPointerException nullException){
-						feedback.setText("") ;
+					try {
+						if (!message.isEmpty()) {
+							feedback.setText((message) + "\n");
+						}
+					} catch (NullPointerException nullException) {
+						feedback.setText("");
 					}
 					try {
 						Display.getInstance().getView().show();
@@ -255,15 +253,11 @@ public class UserInterface {
 					if (wordR == after
 							|| String.valueOf(text.charAt(wordR))
 									.matches("\\W")) {
-						if (text.substring(wordL, wordR)
-								.toLowerCase()
-								.matches(
-										"(\\W)*(add|delete|edit|search|from|to|by)"))
-							setCharacterAttributes(wordL, wordR - wordL, attr,
-									false);
-						else
-							setCharacterAttributes(wordL, wordR - wordL,
-									attrBlack, false);
+						if (text.substring(wordL, wordR).toLowerCase().matches("(\\W)*(add|delete|edit|search|from|to|by)")){
+							setCharacterAttributes(wordL, wordR - wordL, attr,false);}
+						else{
+							setCharacterAttributes(wordL, wordR - wordL,attrBlack, false);
+						}
 						wordL = wordR;
 					}
 					wordR++;
