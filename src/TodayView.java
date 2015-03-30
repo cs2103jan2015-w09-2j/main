16cc6492ab71acb155d1f9b70ca616a72e006ace
gangadevib <gangadevi.balakrishnan@gmail.com>
2015-03-30 13:44:14 +0800

    Updated to reflect current gui design

--- src/TodayView.java
+++ src/TodayView.java
@@ -1,3 +1,5 @@
+//@author A0112715
+
 import java.awt.Color;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -76,14 +78,15 @@ public class TodayView extends SingleView implements View {
 								.replace("PM", "pm") + ")";
 				t = t.toString().replaceAll("\\[", "").replaceAll("\\]", " -");
 				if(isOverdue){
-					appendTasks(Color.GRAY,Color.WHITE, numbering);
-					appendTasks(Color.MAGENTA.darker(),Color.WHITE, tasks);
-					appendTasks(Color.MAGENTA.darker(), Color.WHITE, t+"\n");
+					appendTasks(Color.GRAY,Color.WHITE, false,numbering);
+					appendTasks(Color.RED, Color.WHITE, true, "!");
+					appendTasks(Color.MAGENTA.darker(),Color.WHITE,false, tasks);
+					appendTasks(Color.MAGENTA.darker(), Color.WHITE,false, t+"\n");
 				}
 				else{
-				appendTasks(Color.GRAY,Color.white, numbering);
-				appendTasks(Color.BLUE.darker(),Color.white, tasks);
-				appendTasks(Color.CYAN.darker(),Color.white, t+"\n");
+				appendTasks(Color.GRAY,Color.white,false, numbering);
+				appendTasks(Color.BLUE.darker(),Color.white,false, tasks);
+				appendTasks(Color.CYAN.darker(),Color.white,false, t+"\n");
 				}
 				
 			} else {
@@ -96,14 +99,15 @@ public class TodayView extends SingleView implements View {
 				t = t.toString().replaceAll("\\[", "").replaceAll("\\]", " -");
 				
 				if(isOverdue){
-					appendTasks(Color.GRAY, Color.white, numbering);
-					appendTasks(Color.CYAN.darker(), Color.WHITE, t);
-					appendTasks(Color.BLUE.darker(), Color.WHITE, tasks+"\n");
+					appendTasks(Color.GRAY, Color.white,false, numbering);
+					appendTasks(Color.RED, Color.WHITE, true, "!");
+					appendTasks(Color.CYAN.darker(), Color.WHITE,false, t);
+					appendTasks(Color.BLUE.darker(), Color.WHITE,false, tasks+"\n");
 				}
 				else{
-					appendTasks(Color.GRAY, Color.white, numbering);
-					appendTasks(Color.CYAN.darker(), Color.white, t);
-					appendTasks(Color.BLUE.darker(), Color.white, tasks+"\n");
+					appendTasks(Color.GRAY, Color.white,false, numbering);
+					appendTasks(Color.CYAN.darker(), Color.white,false, t);
+					appendTasks(Color.BLUE.darker(), Color.white,false, tasks+"\n");
 				}
 
 			}
@@ -111,10 +115,11 @@ public class TodayView extends SingleView implements View {
 		}
 	}
 
-	public void appendTasks(Color c, Color bg, String s) throws BadLocationException {
-		StyleConstants.setBold(style, false);
-		StyleConstants.setFontSize(style, 16);
-		StyleConstants.setBackground(style,bg);
+	public void appendTasks(Color c, Color bg, boolean isBold, String s)
+			throws BadLocationException {
+		StyleConstants.setBold(style, isBold);
+		StyleConstants.setFontSize(style, 14);
+		StyleConstants.setBackground(style, bg);
 		StyleConstants.setForeground(style, c);
 		doc.insertString(doc.getLength(), s, style);
 	}
@@ -126,10 +131,9 @@ public class TodayView extends SingleView implements View {
 		StyleConstants.setForeground(style, Color.WHITE);
 		StyleConstants.setBackground(style, new Color(84, 121, 163));
 		StyleConstants.setBold(style, true);
-		StyleConstants.setFontSize(style, 18);
-		doc.insertString(doc.getLength(), "\n  		     Today (" + todayDate
-				+ ")	                   \n", style);
-		doc.insertString(doc.getLength(),"\n",style);
+		StyleConstants.setFontSize(style, 15);
+		doc.insertString(doc.getLength(), "\n  		            Today (" + todayDate
+				+ ")	                                    \n", style);
 		getToday();
 
 	}
