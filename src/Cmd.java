1811fa7e4c91ed8176f8c7bc055287171ccea3ca
Gangadevi <gangadevi.balakrishnan@gmail.com>
2015-04-01 14:33:03 +0800

    Messages updated.

--- src/Cmd.java
+++ src/Cmd.java
@@ -2,22 +2,39 @@
 
 public abstract class Cmd {
 	
-	protected static final String MESSAGE_ADD = "Added to %1$s: “%2$s”";
-	protected static final String MESSAGE_UNDO_ADD = "Undo add";
-	protected static final String MESSAGE_EDIT = "Edited";
-	protected static final String MESSAGE_UNDO_EDIT = "Undo Edited";
-	protected static final String MESSAGE_DELETE = "Delete";
-	protected static final String MESSAGE_UNDO_DELETE = "Undo Delete";
-	protected static final String MESSAGE_COMPLETED = "Completed";
-	protected static final String MESSAGE_UNDO_COMPLETED = "Undo Completed";
+	//task description and header where description is added needs to be returned
+	// i.e. Added "Complete homework" to Today
+	protected static final String MESSAGE_ADD = "Added \"%1$s\" to %2$s”";
+	
+	protected static final String MESSAGE_EDIT_DES = "Description of the task \"%1$s\" is changed to %2$s";
+	protected static final String MESSAGE_EDIT_TIME = "Time of the task \"%1$s\" is changed to %2$s";
+	protected static final String MESSAGE_EDIT_DATE = "Date of the task \"%1$s\" is changed to %2$s";
+	
+	
+	//Instead of an undo message, just send back the edit?
+	protected static final String MESSAGE_UNDO_EDIT_DES = "Undo Edit: Description reverted back to %1$s";
+	protected static final String MESSAGE_UNDO_EDIT_TIME = "Undo Edit: Time is reverted back to %1$s";
+	protected static final String MESSAGE_UNDO_EDIT_DATE = "Undo Edit: Date is reverted back to %1$s";
+
+	protected static final String MESSAGE_UNDO_ADD = "Undo Add: \"%1$s\" is removed from task list ";
+	
+	//i.e. Lecture is added back to today 
+	protected static final String MESSAGE_UNDO_DELETE = "Undo Delete: \"%1$s\" is added back to %2$s";
+	protected static final String MESSAGE_UNDO_COMPLETED = "Undo Complete: \"%1$s\" is marked as undone";
+	
+	//
+	protected static final String MESSAGE_DELETE = "Deleted \"%1$s\"";
+	protected static final String MESSAGE_COMPLETED = "\"%1$s\" marked as completed!";
+
 //	protected static final String MESSAGE_CLEAR = "all content deleted from %1$s";
 //	protected static final String MESSAGE_EMPTY = "%1$s is empty";
-	protected static final String MESSAGE_EMPTY_SEARCH = "Keyword “%1$s” is not found";
-	protected static final String MESSAGE_UNDO = "Undo";
-	protected static final String MESSAGE_UNDO_EMPTY = "nothing to undo";
-	protected static final String MESSAGE_INVALID = "Command is invalid.Please enter a valid command!";
-//	protected static final String MESSAGE_NO_VALUE = "Please indicate a task to %1$s";
-	protected static final String MESSAGE_NO_FILE = "File not available";
+	
+	protected static final String MESSAGE_EMPTY_SEARCH = "Keyword \"%1$s\" is not found";
+	protected static final String MESSAGE_UNDO_EMPTY = "No action to undo";
+	protected static final String MESSAGE_INVALID = "You have entered an invalid command. Enter a valid command or \"Help\" for assistance.";
+
+	protected static final String MESSAGE_NO_VALUE = "Please indicate a valid task description!";
+	protected static final String MESSAGE_NO_FILE = "Storage file is not available";
 
 	protected Data data = Data.getInstance();
 	protected Display display = Display.getInstance();
